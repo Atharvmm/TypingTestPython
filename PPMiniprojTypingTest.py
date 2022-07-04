a9e8cf793c76d5cfc1f91ece3c7d81f054ea9d64
yashmerchant23 <108644453+yashmerchant23@users.noreply.github.com>
2022-07-04 12:29:11 +0530

    Update PPMiniprojTypingTest.py

--- PPMiniprojTypingTest.py
+++ PPMiniprojTypingTest.py
@@ -33,23 +33,22 @@ class Game:
         self.screen = pygame.display.set_mode((self.w,self.h))
         pygame.display.set_caption('Type Speed test')
 
-        def get_sentence(self):
+  def get_sentence(self):
         f = open('sentences.txt').read()
         sentences = f.split('\n')
         sentence = random.choice(sentences)
         return sentence
         
-        def draw_text(self, screen, msg, y ,fsize, color):
+  def draw_text(self, screen, msg, y ,fsize, color):
         font = pygame.font.Font(None, fsize)
         text = font.render(msg, 1,color)
         text_rect = text.get_rect(center=(self.w/2, y))
         screen.blit(text, text_rect)
         pygame.display.update()   
         
-        def run(self):
+  def run(self):
         self.reset_game()
-    
-        self.running=True
+        elf.running=True
         while(self.running):
             clock = pygame.time.Clock()
             self.screen.fill((0,0,0), (50,250,650,50))
@@ -92,8 +91,8 @@ class Game:
                                 pass
             
             pygame.display.update()
-            
-        def show_results(self, screen):
+
+  def show_results(self, screen):
           if(not self.end):
             #To Calculate time
             self.total_time = time.time() - self.time_start
